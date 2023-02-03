c3b12f4dd447463600311be5cd790e28ad8f1217
Oliver <ge89yes@mytum.de>
2023-02-03 17:47:06 +0100

    Add files via upload

---
+++ excavator_simulation/src/environment_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gnome-terminal -- bash -c 
+cd ~/catkin_ws/src/pudong_gazebo/scripts
+chmod +x data_record.py
+chmod +x motion_control.py
+chmod +x number_statistic.py
+chmod +x peijian_move.py
+chmod +x reconstructed_excavator_control.py
+chmod +x row_pitch_control.py
+chmod +x shikuai_shengcheng.py
+chmod +x task2.py
+chmod +x zhuti_move.py
+chmod +x guding_cswj.py
+chmod +x guding_model_position.py
